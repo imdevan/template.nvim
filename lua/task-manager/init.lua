a4a0ff8cd97b72982c4648c1205b22dfae829e6f
Devan Huapaya <huapayadevan@gmail.com>
2026-04-11 10:40:24 -0700

    add init add feature

--- lua/task-manager/init.lua
+++ lua/task-manager/init.lua
@@ -8,6 +8,10 @@ function M.setup(opts)
   vim.api.nvim_create_user_command("TaskToggleCheckbox", function()
     require("task-manager.toggle").toggle_checkbox_cursor()
   end, { desc = "Toggle task/subtask checkbox on the current line" })
+
+  vim.api.nvim_create_user_command("TaskAddFeature", function()
+    require("task-manager.add").add_feature_cursor()
+  end, { desc = "Prompt for a name and insert a new feature at the cursor line" })
 end
 
 return M
