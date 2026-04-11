75b360ab6bbe5348cc8697d86fd2996b8ce6dd0b
Devan Huapaya <huapayadevan@gmail.com>
2026-04-10 23:05:55 -0700

    add init task toggle checkbox

--- lua/task-manager/init.lua
+++ lua/task-manager/init.lua
@@ -4,6 +4,10 @@ local M = {}
 ---@param opts? TaskManagerConfig
 function M.setup(opts)
   require("task-manager.config").setup(opts)
+
+  vim.api.nvim_create_user_command("TaskToggleCheckbox", function()
+    require("task-manager.toggle").toggle_checkbox_cursor()
+  end, { desc = "Toggle task/subtask checkbox on the current line" })
 end
 
 return M
